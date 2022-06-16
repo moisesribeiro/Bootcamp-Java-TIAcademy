93f5e28a07a123051ad17680200b907dbd8a0bb3
Moises Ribeiro <moises.rsjr@gmail.com>
2022-06-15 22:51:10 -0300

    Exercícios de Lógica de Programação em Java - TI Academy

---
+++ Logica/src/sequencial/OperadoresAritmetico.java
@@ -0,0 +1,12 @@
+package sequencial;
+
+public class OperadoresAritmetico {
+
+	public static void main(String[] args) {
+		
+		System.out.println(10 * 5 / 2);
+		System.out.println((25 + 10) * 4 / 2 );
+
+	}
+
+}
---
+++ Logica/src/sequencial/Variaveis.java
@@ -0,0 +1,19 @@
+package sequencial;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int quantidade = 1500;
+		float valorMercadoria = 750.28f;
+		double peso = 78.3;
+		char vendido = 'S';
+		
+		System.out.printf("A quantidade de itens em estoque é %d\n", quantidade);
+		System.out.printf("O valor da parcela é R$%.1f\n", valorMercadoria);
+		System.out.printf("O seu Gerônimo pesa %f quilos\n", peso);
+		System.out.printf("Temos uma quantidade %c de produtos (%d - 500) em estoque\n", vendido, quantidade);
+
+	}
+
+}
